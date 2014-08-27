d5f230bffd4f64de39016ced0c61cbb06b2d53c5
woofshahenzup <copymanuals2@gmail.com>
2014-08-27 19:57:11 -0300

    gftp removido

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 ## Este script contiene los archivos y aplicaciones que se 
 ## van a remover de la .iso base
+## por nilsomorales y Max_escorpion
 
 rm /usr/share/applications/Gftp-ftp-client.desktop
+rm /etc/gftp/gftprc
+rm /usr/bin/gftp
+rm -r /usr/share/gftp
+### gftp removido ###
